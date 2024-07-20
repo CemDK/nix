29bbb961f254cbf7205a0fc4f675cf33bb4a6cfa
CemDK <25245902+CemDK@users.noreply.github.com>
2024-07-21 01:08:13 +0200

    add tmux config

--- users/cem/home.nix
+++ users/cem/home.nix
@@ -11,6 +11,7 @@
   # User-specific file
   home.file.".config/.p10k-rainbow.zsh".source = ../../modules/home-manager/dotfiles/.p10k-rainbow.zsh;
   home.file.".config/yabai/yabairc".source = ../../modules/home-manager/dotfiles/yabairc;
+  home.file.".config/tmux/tmux.conf".source = ../../modules/home-manager/dotfiles/tmux/tmux.conf;
 
   # Any other user-specific settings
 }
