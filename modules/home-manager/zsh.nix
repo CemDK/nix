cc775555d5074d8d05d58cb11db678069ea2a711
CemDK <25245902+CemDK@users.noreply.github.com>
2024-07-23 16:10:03 +0200

    update zsh configuration

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -7,23 +7,35 @@
     syntaxHighlighting.enable = true;
     dotDir = ".config/zsh";
     shellAliases = import ./aliases.nix;
+    history = {
+        path = "$HOME/.config/zsh/.zsh_history";
+        ignorePatterns = [ "cd" "cd *" "pwd" "ls *" "l" "la" "ll" "lla" "lt" "exit"];
+        save = 10001;
+        size = 10000;
+        share = true;
+
+        expireDuplicatesFirst = true;
+        ignoreDups = true;
+    };
+
+    historySubstringSearch = {
+        # bindkey "^[[A" history-beginning-search-backward
+        # bindkey "^[[B" history-beginning-search-forward
+        enable = true;
+        searchUpKey = [ "^[[A" "^P"];
+        searchDownKey = [ "^[[B" "^N"];
+    };
 
     initExtra = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       source ${./dotfiles/.p10k-rainbow.zsh}
 
-      export ZSH_COMPDUMP=$HOME/.cache/.zcompdump-$HOST
-      SAVEHIST=10000
-      HISTSIZE=9999
-      HISTFILE=$HOME/.config/zsh/.zsh_history
-      setopt share_history
-      setopt hist_expire_dups_first
-      setopt hist_ignore_dups
-      setopt hist_verify
-      bindkey "^[[A" history-search-backward
-      bindkey "^[[B" history-search-forward
-
+      eval "$(/opt/homebrew/bin/brew shellenv)"
       eval "$(zoxide init zsh)"
+
+      export ZSH_COMPDUMP=$HOME/.cache/.zcompdump-$HOST
+
+      setopt hist_verify
     '';
   };
 }
