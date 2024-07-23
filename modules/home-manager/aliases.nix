a05215b0d0a1b45369b0f343c252a6f580aeadce
CemDK <CemDenizKabakci@outlook.com>
2024-07-23 20:49:52 +0200

    fix nixswitch alias

--- modules/home-manager/aliases.nix
+++ modules/home-manager/aliases.nix
@@ -1,7 +1,7 @@
 {
-  nixrebuild = "darwin-rebuild build --flake ~/.config/nix/.#$(hostname)";
+  nixrebuild = "darwin-rebuild build --flake ~/.config/nix/.#$(hostname -s)";
   #nixswitch = "nix run nix-darwin -- switch --flake ~/.config/nix/.#$(hostname)";
-  nixswitch = "darwin-rebuild switch --flake ~/.config/nix/.#$(hostname)";
+  nixswitch = "darwin-rebuild switch --flake ~/.config/nix/.#$(hostname -s)";
   nixup = "pushd ~/.config/nix; nix flake update; nixswitch; popd";
   cat = "bat";
   l = "ll";
