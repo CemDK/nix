313b89275f234799c02aa9076be6f65b23bc7f16
CemDK <25245902+CemDK@users.noreply.github.com>
2024-07-23 16:09:42 +0200

    update aliases

--- modules/home-manager/aliases.nix
+++ modules/home-manager/aliases.nix
@@ -1,12 +1,20 @@
 {
-  nixrebuild = "darwin-rebuild build --flake ~/.config/nix/.#CemDK-MBP";
-  #nixswitch = "nix run nix-darwin -- switch --flake ~/.config/nix";
-  nixswitch = "darwin-rebuild switch --flake ~/.config/nix/";
+  nixrebuild = "darwin-rebuild build --flake ~/.config/nix/.#$(hostname)";
+  #nixswitch = "nix run nix-darwin -- switch --flake ~/.config/nix/.#$(hostname)";
+  nixswitch = "darwin-rebuild switch --flake ~/.config/nix/.#$(hostname)";
   nixup = "pushd ~/.config/nix; nix flake update; nixswitch; popd";
   cat = "bat";
   l = "ll";
+  la = "lla";
   lt = "ll --tree";
   cd = "z";
   vif = "vi $(fzf --preview=\"bat --color=always {}\")";
   cdf = "cd $(fd --type directory | fzf --preview \"eza --tree --colour=always {}\")";
+
+  tadev = "ENV=dev terragrunt apply";
+  tddev = "ENV=dev terragrunt destroy";
+  taprod = "ENV=prod terragrunt apply";
+  tdprod = "ENV=prod terragrunt destroy";
+
+  vimtip = "curl -w \"\n\" https://vtip.43z.one";
 }
