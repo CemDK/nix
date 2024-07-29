fab41c8d81f89b3855f02a8fc387d948a4c11552
CemDK <CemDenizKabakci@outlook.com>
2024-07-29 20:10:52 +0200

    update colors

--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -5,19 +5,20 @@
     defaultCommand = "rg --files --hidden --follow --glob '!.git/*'";
     defaultOptions = [
       "--layout=reverse"
-      "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
       "--preview-window=up,70%"
       "--bind 'ctrl-/:toggle-preview'"
     ];
+      #"--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+
     colors = {
       "fg"="#93a1a1";
       "hl"="#268bd2";
       "fg+"="#c5c8c6";
       "bg+"="#005F60";
-      "hl+"="#83a598";
+      "hl+"="#8ec07c";
       "info"="#2aa198";
-      "prompt"="#fd5901";
-      "pointer"="#fd5901";
+      "prompt"="#f78104";
+      "pointer"="#f78104";
       "marker"="#f78104";
       "spinner"="#f78104";
       "header"="#268bd2";
