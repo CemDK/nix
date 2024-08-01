cc509469941b6fb723f6bc19a25f8a202ea90d28
CemDK <CemDenizKabakci@outlook.com>
2024-08-01 22:42:18 +0200

    update nvim config

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -65,10 +65,33 @@
     
     plugins = {
       lightline.enable = true;
-      # nvim-tree.enable = true;
+      treesitter = {
+        enable = true;
+        nodejsPackage = null;
+        gccPackage = null;
+        nixGrammars = true;
+        grammarPackages = with pkgs.vimPlugins; [
+          nvim-treesitter-parsers.c
+          nvim-treesitter-parsers.markdown
+          nvim-treesitter-parsers.nix
+          nvim-treesitter-parsers.rust
+          nvim-treesitter-parsers.terraform
+          nvim-treesitter-parsers.toml
+          nvim-treesitter-parsers.typescript
+          nvim-treesitter-parsers.vim
+          nvim-treesitter-parsers.vimdoc
+          nvim-treesitter-parsers.yaml
+        ];
+
+        settings = {
+          auto_install = true;
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
     };
     
-    #colorschemes.gruvbox.enable = true;
+    colorscheme = "solarized";
 
     extraPlugins = with pkgs.vimPlugins; [
       {
@@ -79,7 +102,26 @@
         plugin = fzf-lua;
         config = ''lua require("fzf-lua").setup({"fzf-vim"})''; 
       }
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "oat-nvim";
+        src = pkgs.fetchFromGitHub {
+            owner = "CemDK";
+            repo = "oat.nvim";
+            rev = "b6c501bf93db1de8eb5febf1013fd32023bcaa9f";
+            hash = "sha256-lAN0hRpyloNZlft1ht8A7MNdanxJLi0RXWThobZO2Hg=";
+        };
+      })
     ];
-    
+
+    extraConfigLuaPre = ''
+        require('solarized').setup({ 
+          transparent = true, 
+          palette = 'solarized' 
+        })
+      '';
+
+    extraConfigLuaPost = ''
+      '';
+
   };
 }
