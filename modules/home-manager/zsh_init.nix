549d298917032aa0baae4a393882e61a72b891cf
CemDK <25245902+CemDK@users.noreply.github.com>
2025-04-12 22:51:10 +0200

    update tmux.conf, update nixswitch alias

--- modules/home-manager/zsh_init.nix
+++ modules/home-manager/zsh_init.nix
@@ -1,10 +1,11 @@
 ''
   if [[ $(uname) == "Darwin" ]]; then
     alias activate-settings="/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u"
-    alias nixswitch="darwin-rebuild switch --flake ~/.config/nix/.#$(whoami)@work && activate-settings"
     if [[ $(uname -m) == "arm64" ]]; then
+      alias nixswitch="darwin-rebuild switch --flake ~/.config/nix/.#$(whoami)@work && activate-settings"
       eval "$(/opt/homebrew/bin/brew shellenv)"
     else
+      alias nixswitch="darwin-rebuild switch --flake ~/.config/nix/.#$(whoami)@$(hostname -s) && activate-settings"
       eval "$(/usr/local/bin/brew shellenv)"
     fi
   else
