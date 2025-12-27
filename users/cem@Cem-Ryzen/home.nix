a89fb56533c73caf24e215302f1833c3ea829772
Cem Deniz Kabakci <CemDenizKabakci@outlook.com>
2025-12-27 18:52:02 +0100

    update

--- users/cem@Cem-Ryzen/home.nix
+++ users/cem@Cem-Ryzen/home.nix
@@ -3,13 +3,7 @@
 {
   imports = [ ../../modules/home-manager/default.nix ];
 
-  # TODO: re-evaluate if these are necessary
   systemd.user.startServices = "sd-switch";
-  # WSL-specific settings
-  # home.sessionVariables = {
-  #   DISPLAY = "$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0";
-  #   LIBGL_ALWAYS_INDIRECT = "1";
-  # };
 
   home = {
     username = user;
