880fbe2de7da4556726065b5fd9d5e2d51aaeef4
CemDK <25245902+CemDK@users.noreply.github.com>
2026-04-10 23:13:13 +0200

    update

--- hosts/darwin/mac-mini/packages.nix
+++ hosts/darwin/mac-mini/packages.nix
@@ -43,26 +43,6 @@
   # ============================================================================
   # OTHER CONFIGURATIONS
   # ============================================================================
-  # TODO: not working
-  # This will add better display, hidden-bar, etc. shortucts into /Applications/Nix Apps/
-  # system.activationScripts.applications.text = let
-  #   env = pkgs.buildEnv {
-  #     name = "system-applications";
-  #     paths = config.environment.systemPackages;
-  #     pathsToLink = "/Applications";
-  #   };
-  # in pkgs.lib.mkForce ''
-  #   # Set up applications.
-  #   echo "setting up /Applications..." >&2
-  #   rm -rf /Applications/Nix\ Apps
-  #   mkdir -p /Applications/Nix\ Apps
-  #   find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  #   while read -r src; do
-  #       app_name=$(basename "$src")
-  #       echo "copying $src" >&2
-  #       ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  #   done
-  # '';
 
   # system.activationScripts.applications.text = ''
   #   set -e
@@ -72,4 +52,5 @@
   #     /Users/cemdk/.config/wallpapers/autumn-season-leafs-plant-scene-generative-ai.jpg
   #   }\""
   # '';
+
 }
