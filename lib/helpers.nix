c7c6c84546ac65db64d0a3a5a850c044945c38dc
CemDK <25245902+CemDK@users.noreply.github.com>
2026-04-27 17:49:03 +0200

    update

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -8,9 +8,9 @@
 
   mapModules =
     dir: fn:
-    lib.mapAttrs' (
-      name: _: lib.nameValuePair (lib.removeSuffix ".nix" name) (fn (dir + "/${name}"))
-    ) (lib.filterAttrs (n: _: lib.hasSuffix ".nix" n) (builtins.readDir dir));
+    lib.mapAttrs' (name: _: lib.nameValuePair (lib.removeSuffix ".nix" name) (fn (dir + "/${name}"))) (
+      lib.filterAttrs (n: _: lib.hasSuffix ".nix" n) (builtins.readDir dir)
+    );
 
   format = {
     header = text: "echo \$'\\n\\e[1m'${text}\$'\\e[0m'";
