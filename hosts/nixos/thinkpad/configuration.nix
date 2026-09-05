6553a438cb12258a7f79c47eb073e21e4ab7c95b
CemDK <25245902+CemDK@users.noreply.github.com>
2026-09-05 20:37:57 +0200

    update

--- hosts/nixos/thinkpad/configuration.nix
+++ hosts/nixos/thinkpad/configuration.nix
@@ -131,20 +131,6 @@ in
   # input.touchpad.disable_while_typing = false;
 
   # ============================================================================
-  # NIX SETTINGS
-  # ============================================================================
-  nix.settings = {
-    extra-substituters = [
-      "https://walker.cachix.org"
-      "https://walker-git.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
-      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
-    ];
-  };
-
-  # ============================================================================
   # SYSTEM
   # ============================================================================
   system.stateVersion = "25.05";
--- modules/profiles/desktop/default.nix
+++ modules/profiles/desktop/default.nix
@@ -9,13 +9,21 @@
 
   home-manager.sharedModules = [ "${self}/modules/home/walker" ];
 
-  programs.firefox.enable = true;
-
-  programs.nix-ld.enable = true;
+  nix.settings = {
+    extra-substituters = [
+      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+    ];
+  };
 
   fonts.packages = [ pkgs.nerd-fonts.meslo-lg ];
 
-  # enable zsh and set it as default user shell
+  programs.firefox.enable = true;
+  programs.nix-ld.enable = true;
   programs.zsh.enable = true;
   # home-manager runs compinit already; a second run in /etc/zshrc costs ~120ms per shell.
   # But enableCompletion also linked /share/zsh into the system profile, which running
